941b3b3022253eeda84b6ec6e31a9ef0e0345c64
BeBrandao <beabs2305@gmail.com>
2024-11-01 10:13:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -59,3 +59,29 @@ for(z=0; z<usuarios.length; z++) {
    var nomes = document.getElementById("corpo");
    nomes.appendChild(exibir);
 }
+
+/*Questão 6*/
+
+var contagem = [
+    {imagem:'joia.jpg', texto: 'Acabou'},
+    {imagem:'um.jpg', texto: 'Um'},
+    {imagem:'dois.jpg', texto: 'Dois'},
+    {imagem:'tres.jpg', texto: 'Três'},
+    {imagem:'quatro.jpg', texto: 'Quatro'},
+    {imagem:'cinco.jpg', texto: 'Cinco'}
+];
+
+var indice = contagem.length - 1;
+escrita = "Acabou"
+function regressiva(){
+    if(indice < 0) {
+        return false;
+    }
+    var img = document.querySelector("#contador img");
+    var div = document.querySelector("#contador div");
+    img.src = contagem[indice].imagem;
+    div.innerHTML = contagem[indice].texto;
+    indice = indice - 1;
+}
+
+setInterval(regressiva, 1500);
